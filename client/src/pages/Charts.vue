5f32ffdde3446acfbf2c65cb8c9c32b612409fb7
Egor Kuzmin <ekuzmin@kraftway.ru>
2020-02-19 16:40:02 +0300

    [add]: Отображение графиков

--- client/src/pages/Charts.vue
+++ client/src/pages/Charts.vue
@@ -1,11 +1,108 @@
 <template>
-  <q-page class="flex flex-center">
-    <span style="text-align: center">Charts</span>
+  <q-page>
+    <apexchart type="scatter" :height="height" width="100%" :series="items" :options="options"/>
+
+    <q-page-sticky position="bottom-right" :offset="[12, 12]">
+      <q-fab direction="up" icon="list" color="primary">
+        <q-fab-action :outline="!(itemsType === 'all')" @click="itemsType = 'all'" external-label label-position="left"
+                      color="primary" icon="font_download" label="Все"/>
+        <q-fab-action :outline="!(itemsType === 'autoru')" @click="itemsType = 'autoru'" external-label
+                      label-position="left" color="primary" icon="directions_car" label="Auto.ru"/>
+        <q-fab-action :outline="!(itemsType === 'avito')" @click="itemsType = 'avito'" external-label
+                      label-position="left" color="primary" icon="directions_car" label="Avito"/>
+      </q-fab>
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'
+
 export default {
-  name: 'Charts'
+  name: 'Charts',
+  methods: {
+    isPortait () {
+      return screen.orientation.type.includes('portrait')
+    }
+  },
+  data () {
+    return {
+      itemsType: 'all',
+      height: this.isPortait() ? '90%' : '200%',
+      isRender: true,
+      options: {
+        chart: {
+          toolbar: {
+            show: false
+          }
+        },
+        xaxis: {
+          tickAmount: 10,
+          title: {
+            text: 'Цена, ₽'
+          }
+        },
+        yaxis: {
+          tickAmount: this.isPortait() ? 10 : 20,
+          title: {
+            text: 'Пробег, км'
+          }
+        },
+        tooltip: {
+          x: {
+            formatter: (val) => `${val} ₽`
+          },
+          y: {
+            formatter: (val) => `${val} км`,
+            title: {
+              formatter: () => 'Пробег'
+            }
+          }
+        }
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['ads', 'avitoAds', 'autoRuAds']),
+    ...mapState(['favorite']),
+    items () {
+      let source = []
+      switch (this.itemsType) {
+        case 'avito':
+          source = this.avitoAds
+          break
+        case 'autoru':
+          source = this.autoRuAds
+          break
+        default:
+          source = this.ads
+      }
+
+      // разделяем обычные точки и избранные
+      const favorite = [], other = []
+      for (const ad of source) {
+        if (!this.favorite.includes(ad.id)) {
+          other.push([ad.price, ad.mileage])
+        } else {
+          favorite.push([ad.price, ad.mileage])
+        }
+      }
+
+      return [{
+        name: 'Обычные',
+        data: other
+      }, {
+        name: 'Избранные',
+        data: favorite
+      }]
+    }
+  },
+  created () {
+    window.addEventListener('orientationchange', () => {
+      const isPortait = this.isPortait()
+      this.options.yaxis.tickAmount = isPortait ? 10 : 20
+      this.height = isPortait ? '90%' : '100%'
+    })
+  }
 }
 </script>
