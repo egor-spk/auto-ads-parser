8addc5842b4199d083f286f1f4b950dbc778986c
Egor Kuzmin <egor.spk@gmail.com>
2020-04-05 23:57:02 +0300

    [fix]: обработка кода 301

--- server/common/src/parser/include/CurlTransport.h
+++ server/common/src/parser/include/CurlTransport.h
@@ -6,6 +6,7 @@
 #include <restclient-cpp/restclient.h>
 #include <fmt/format.h>
 #include <restclient-cpp/connection.h>
+#include <regex>
 
 namespace parser
 {
@@ -14,26 +15,52 @@ namespace parser
     public:
         CurlTransport() = default;
 
-        CurlTransport(const std::unordered_map<std::string, std::string> &cookie)
+        explicit CurlTransport(const std::unordered_map<std::string, std::string> &cookie)
                 : cookie_(cookieToStr(cookie)) {}
 
         std::string get(const std::string &link) override
         {
-            auto conn = std::make_unique<RestClient::Connection>(link);
-            if (!cookie_.empty())
+            RestClient::Response response;
+            bool repeatRequest{false};
+            uint8_t requestCount{};
+            auto link_ = link;
+
+            do
             {
-                conn->AppendHeader("Cookie", cookie_);
-            }
+                ++requestCount;
+                auto conn = std::make_unique<RestClient::Connection>(link_);
+                if (!cookie_.empty())
+                {
+                    conn->AppendHeader("Cookie", cookie_);
+                }
 
-            RestClient::Response r = conn->get("");
+                response = conn->get("");
+                if (response.code == 301 && requestCount < 3)
+                {
+                    const auto &headers = response.headers;
+                    const char *LocationFieldName = "Location";
+                    if (headers.find(LocationFieldName) == headers.cend())
+                    {
+                        throw WebSiteTransportError(fmt::format("Got 301 response code from \"{}\""
+                                                                ", but unable to find \"Location\" header field.",
+                                                                link_));
+                    }
 
-            if (r.code != 200)
+                    link_ = fixSiteUrl(link_, headers.at(LocationFieldName));
+                    repeatRequest = true;
+                } else
+                {
+                    repeatRequest = false;
+                }
+            } while (repeatRequest);
+
+            if (response.code != 200)
             {
                 throw WebSiteTransportError(fmt::format("Unable to fetch \"{}\". Error {}: {}",
-                                                        link, r.code, r.body));
+                                                        link, response.code, response.body));
             }
 
-            return r.body;
+            return response.body;
         }
 
     private:
@@ -45,6 +72,30 @@ namespace parser
                 res += fmt::format("{}={};", key, value);
             }
             return res;
+        }
+
+        /**
+         * @brief Добавляет протокол и адрес сайта, если в новом url его нет
+         * @param originUrl полный url сайта
+         * @param newUrl новый url
+         * @return url с протоколом и адресом сайта
+         */
+        std::string fixSiteUrl(const std::string &originUrl, const std::string &newUrl)
+        {
+            // если новый url начинается со слеша, то требуется добавить протокол и адрес сайта
+            if (*newUrl.cbegin() != '/')
+                return newUrl;
+
+            // получаем недостающую часть
+            static const std::regex reg{R"(^https?\:\/\/[^\/]+)", std::regex_constants::icase};
+            std::smatch match;
+            if (!std::regex_search(originUrl, match, reg))
+            {
+                throw WebSiteTransportError(fmt::format("Unable find site and protocol in origin url: {}",
+                                                        originUrl));
+            }
+
+            return match[0].str() + newUrl;
         }
 
         std::string cookie_;
